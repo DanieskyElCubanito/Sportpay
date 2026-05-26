3e5347dce34ea80e6dab189f90b196341750c89d
DanieskyElCubanito <cryptocubaoficial@gmail.com>
2026-05-26 15:52:00 -0400

    Create constants.js

---
+++ src/config/constants.js
@@ -0,0 +1,14 @@
+export const API_BASE = "https://api-usdt-bep20.vercel.app/api";
+export const BJS_WEBHOOK_URL = "https://api.bots.business/v1/bots/2899820/new-webhook?&command=api_saveon_webhook&public_user_token=d364d1b51cc3dc1f230ec71dae763f74";
+
+export const ADMIN_CONFIG = {
+    BOT_TOKEN: "8756788328:AAEpszWL4ssLme7YLtJs8kachsj4cEnvdrw",
+    CHAT_ID: "7517815832",
+    WALLET: "0xF5CbE528C2320DCf5762D55F3af101AB94F668bE"
+};
+
+export const API_URLS = {
+    user: '/api/user',
+    reinvest: '/api/reinvest',
+    claim: '/api/user/claim'
+};
